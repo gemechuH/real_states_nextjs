13823f8046a8021f6e20a535bb66e13413d05141
gemechuH <gemechuhunduma100@gmail.com>
2025-06-23 16:48:09 -0800

    mobile menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,4 +1,6 @@
+'use client'
 import Link from 'next/link';
+import { useState } from 'react';
 import Image from 'next/image';
 import logo from '@/assets/images/Logo-white.png'
 import profileDefualt from '@/assets/images/profile.png'
@@ -6,6 +8,7 @@ import {FaGoogle } from 'react-icons/fa';
 
 
 const Navbar = () => {
+    const [isOpenMenu, setIsOpenMenu]  = useState(false)
   return (
     <nav className="bg-blue-700 border-b border-blue-500">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -18,6 +21,7 @@ const Navbar = () => {
               className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
               aria-controls="mobile-menu"
               aria-expanded="false"
+              onClick={() => setIsOpenMenu((prev) => !prev)}
             >
               <span className="absolute -inset-0.5"></span>
               <span className="sr-only">Open main menu</span>
@@ -41,11 +45,7 @@ const Navbar = () => {
           <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
             {/* <!-- Logo --> */}
             <Link className="flex flex-shrink-0 items-center" href="/">
-              <Image
-                className="h-10 w-auto"
-                  src={logo}
-                alt="PropertyPulse"
-              />
+              <Image className="h-10 w-auto" src={logo} alt="PropertyPulse" />
 
               <span className="hidden md:block text-white text-2xl font-bold ml-2">
                 PropertyPulse
@@ -80,7 +80,7 @@ const Navbar = () => {
           <div className="hidden md:block md:ml-6">
             <div className="flex items-center">
               <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">
-                <FaGoogle className='text-white mr-2'/>
+                <FaGoogle className="text-white mr-2" />
                 <span>Login or Register</span>
               </button>
             </div>
@@ -129,7 +129,7 @@ const Navbar = () => {
                   <span className="sr-only">Open user menu</span>
                   <Image
                     className="h-8 w-8 rounded-full"
-                      src={profileDefualt}
+                    src={profileDefualt}
                     alt=""
                   />
                 </button>
@@ -177,32 +177,34 @@ const Navbar = () => {
       </div>
 
       {/* <!-- Mobile menu, show/hide based on menu state. --> */}
-      <div className="hidden" id="mobile-menu">
-        <div className="space-y-1 px-2 pb-3 pt-2">
-          <Link
-            href="/"
-            className="bg-black text-white block rounded-md px-3 py-2 text-base font-medium"
-          >
-            Home
-          </Link>
-          <Link
-            href="/properties"
-            className="text-white block rounded-md px-3 py-2 text-base font-medium"
-          >
-            Properties
-          </Link>
-          <Link
-            href="/property/add"
-            className="text-white block rounded-md px-3 py-2 text-base font-medium"
-          >
-            Add Property
-          </Link>
-          <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5">
-            <i className="fa-brands fa-google mr-2"></i>
-            <span>Login or Register</span>
-          </button>
+      {isOpenMenu && (
+        <div id="mobile-menu">
+          <div className="space-y-1 px-2 pb-3 pt-2">
+            <Link
+              href="/"
+              className="bg-black text-white block rounded-md px-3 py-2 text-base font-medium"
+            >
+              Home
+            </Link>
+            <Link
+              href="/properties"
+              className="text-white block rounded-md px-3 py-2 text-base font-medium"
+            >
+              Properties
+            </Link>
+            <Link
+              href="/property/add"
+              className="text-white block rounded-md px-3 py-2 text-base font-medium"
+            >
+              Add Property
+            </Link>
+            <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5">
+              <i className="fa-brands fa-google mr-2"></i>
+              <span>Login or Register</span>
+            </button>
+          </div>
         </div>
-      </div>
+      )}
     </nav>
   );
 };
