73b45477b86e7b962167957b591266c0759aeb64
gemechuH <gemechuhunduma100@gmail.com>
2025-06-25 22:32:43 -0800

    user model for database

--- models/User.js
+++ models/User.js
@@ -8,5 +8,23 @@ const UserSchema = new Schema({
         required: [true, 'Email is required']
         
     },
+    userName: {
+        type: String,
+        required: [true, 'userName is required'],
 
+    },
+    image: {
+        type: String,
+
+    },
+    bookmarks: [{
+        type: Schema.Types.ObjectId,
+        ref: 'property'
+    }]
+
+
+}, {
+    timestamps: true
 })
+const User = models.User || model('User', UserSchema)
+ export default User
