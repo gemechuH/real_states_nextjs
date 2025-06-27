db47dba5b2275e6e09ab87a202cbe5469650662d
gemechuH <gemechuhunduma100@gmail.com>
2025-06-26 17:14:54 -0800

    single page data

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,7 +1,10 @@
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
 
-
-const idpage = ({ params, searchParams }) => {
-  return <div>id page {searchParams.name} </div>;
+const propertyPageById = async({ params }) => {
+  await connectDB()
+  const property = await Property.findById(params.id).lean()
+  return <div>{property.name} </div>;
 }
 
-export default idpage
+export default propertyPageById
