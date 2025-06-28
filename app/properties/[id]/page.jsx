f99b0e8e0caa6329d6eb25d187c6ded41196a5f7
gemechuH <gemechuhunduma100@gmail.com>
2025-06-27 16:30:16 -0800

    single page style

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,6 +1,8 @@
 import PropertySinglePageImage from "@/components/PropertySinglePageImage";
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
+import Link from "next/link";
+import {FaArrowLeft} from 'react-icons/fa'
 
 const propertyPageById = async({ params }) => {
   await connectDB()
@@ -10,12 +12,12 @@ const propertyPageById = async({ params }) => {
       <PropertySinglePageImage image={property.images[0]} />
       <section>
         <div class="container m-auto py-6 px-6">
-          <a
-            href="/properties.html"
+          <Link
+            href="/properties"
             class="text-blue-500 hover:text-blue-600 flex items-center"
           >
-            <i class="fas fa-arrow-left mr-2"></i> Back to Properties
-          </a>
+            <FaArrowLeft className="mr-2"/> to Properties
+          </Link>
         </div>
       </section>
       <div>{property.name} </div>
