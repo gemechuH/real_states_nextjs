898df6de608dfb5cb96f1bc163189998d211d98a
gemechuH <gemechuhunduma100@gmail.com>
2025-06-27 17:43:05 -0800

    added

--- components/PropertyDetailSinglePage.jsx
+++ components/PropertyDetailSinglePage.jsx
@@ -1,4 +1,4 @@
-import {FaTimes, FaBed, FaBath} from 'react-icons/fa'
+import {FaTimes, FaBed, FaBath, FaRulerCombined, FaMapMarker, FaCheck} from 'react-icons/fa'
 const PropertyDetailSinglePage = ({ property }) => {
     return (
       <main>
@@ -6,7 +6,7 @@ const PropertyDetailSinglePage = ({ property }) => {
           <div className="text-gray-500 mb-4">{property.type}</div>
           <h1 className="text-3xl font-bold mb-4">{property.name}</h1>
           <div className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start">
-            <i className="fa-solid fa-location-dot text-lg text-orange-700 mr-2"></i>
+            <FaMapMarker className="text-orange-400 mr-1 mt-1" />
             <p className="text-orange-700">
               {property.location.street}, {property.location.city},{" "}
               {property.location.state}, {property.location.zip}
@@ -21,7 +21,7 @@ const PropertyDetailSinglePage = ({ property }) => {
               <div className="text-gray-500 mr-2 font-bold">Nightly</div>
               <div className="text-2xl font-bold text-blue-500">
                 {property.rates.nightly ? (
-                  `$${property.rates.nightly}`
+                  `$${property.rates.nightly.toLocaleString()}`
                 ) : (
                   <FaTimes className="text-red-700" />
                 )}
@@ -31,7 +31,7 @@ const PropertyDetailSinglePage = ({ property }) => {
               <div className="text-gray-500 mr-2 font-bold">Weekly</div>
               <div className="text-2xl font-bold text-blue-500">
                 {property.rates.weekly ? (
-                  `$${property.rates.weekly}`
+                  `$${property.rates.weekly.toLocaleString()}`
                 ) : (
                   <FaTimes className="text-red-700" />
                 )}
@@ -41,7 +41,7 @@ const PropertyDetailSinglePage = ({ property }) => {
               <div className="text-gray-500 mr-2 font-bold">Monthly</div>
               <div className="text-2xl font-bold text-blue-500">
                 {property.rates.monthly ? (
-                  `$${property.rates.monthly}`
+                  `$${property.rates.monthly.toLocaleString()}`
                 ) : (
                   <FaTimes className="text-red-700" />
                 )}
@@ -54,81 +54,30 @@ const PropertyDetailSinglePage = ({ property }) => {
           <h3 className="text-lg font-bold mb-6">Description & Details</h3>
           <div className="flex justify-center gap-4 text-blue-500 mb-4 text-xl space-x-9">
             <p>
-              <FaBed className='inline'/> {property.beds}
-              <span className="hidden sm:inline">Beds</span>{' '}
+              <FaBed className="inline-block mr-2" /> {property.beds}{' '}
+              <span className="hidden sm:inline">Beds</span>{" "}
             </p>
             <p>
-              <FaBath className='inline'/> {property.baths}{' '}
+              <FaBath className="inline-block mr-2" /> {property.baths}{" "}
               <span className="hidden sm:inline">Baths</span>
             </p>
             <p>
-              <i className="fa-solid fa-ruler-combined"></i>{" "}
-              {property.square_feet}{" "}
+              <FaRulerCombined className='inline-block mr-2' /> {property.square_feet}{" "}
               <span className="hidden sm:inline">sqft</span>
             </p>
           </div>
-          <p className="text-gray-500 mb-4">
-            This is a beautiful apartment located near the commons
-          </p>
+          <p className="text-gray-500 mb-4">{property.description}</p>
         </div>
 
         <div className="bg-white p-6 rounded-lg shadow-md mt-6">
           <h3 className="text-lg font-bold mb-6">Amenities</h3>
 
           <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 list-none">
-            <li>
-              <i className="fas fa-check text-green-600 mr-2 mt-3"></i> Wifi
-            </li>
-            <li>
-              <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Full
-              kitchen
-            </li>
-            <li>
-              <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Washer &
-              Dryer
-            </li>
-            <li>
-              <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Free
-              Parking
-            </li>
-            <li>
-              <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Hot Tub
-            </li>
-            <li>
-              <i className="fas fa-check text-green-600 mr-2 mt-3"></i>24/7
-              Security
-            </li>
-            <li>
-              <i className="fas fa-check text-green-600 mr-2 mt-3"></i>
-              Wheelchair Accessible
-            </li>
-            <li>
-              <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Elevator
-              Access
-            </li>
-            <li>
-              <i className="fas fa-check text-green-600 mr-2 mt-3"></i>
-              Dishwasher
-            </li>
-            <li>
-              <i className="fas fa-check text-green-600 mr-2 mt-3"></i>
-              Gym/Fitness Center
-            </li>
-            <li>
-              <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Air
-              Conditioning
-            </li>
-            <li>
-              <i className="fas fa-check text-green-600 mr-2 mt-3"></i>
-              Balcony/Patio
-            </li>
-            <li>
-              <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Smart TV
-            </li>
-            <li>
-              <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Coffee
-              Maker
-            </li>
+            {property.amenities.map((amenity, index) => (
+              <li key={index}>
+                <FaCheck className='inline-block mr-1 text-green-400'/> {amenity}
+              </li>
+            ))}
           </ul>
         </div>
         {/* <!-- Map --> */}
