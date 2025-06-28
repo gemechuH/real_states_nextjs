b2b9fc5f7ee0fb927c6eee14dd1024971147ee86
gemechuH <gemechuhunduma100@gmail.com>
2025-06-27 16:25:56 -0800

    make changed

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,10 +1,26 @@
+import PropertySinglePageImage from "@/components/PropertySinglePageImage";
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
 
 const propertyPageById = async({ params }) => {
   await connectDB()
   const property = await Property.findById(params.id).lean()
-  return <div>{property.name} </div>;
+  return (
+    <>
+      <PropertySinglePageImage image={property.images[0]} />
+      <section>
+        <div class="container m-auto py-6 px-6">
+          <a
+            href="/properties.html"
+            class="text-blue-500 hover:text-blue-600 flex items-center"
+          >
+            <i class="fas fa-arrow-left mr-2"></i> Back to Properties
+          </a>
+        </div>
+      </section>
+      <div>{property.name} </div>
+    </>
+  );
 }
 
 export default propertyPageById
