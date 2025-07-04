f2e5ac87e46853fb65cfb436ed20a22e60eb402c
gemechuH <gemechuhunduma100@gmail.com>
2025-07-04 03:53:20 +0300

    added

--- components/ProfileProperties.jsx
+++ components/ProfileProperties.jsx
@@ -1,5 +1,6 @@
 'use client'
 import { useState } from "react";
+import Image from "next/image";
 
 const ProfilePropeties = ({ properties: intialProperties }) => {
 const [properties, setproperties] = useState(intialProperties)
@@ -7,15 +8,17 @@ const [properties, setproperties] = useState(intialProperties)
       <>
         <div className="mb-10">
           <a href="/property.html">
-            <img
+            <Image
               className="h-32 w-full rounded-md object-cover"
-              src="/images/properties/a1.jpg"
+                        src={property.images[0]}
+                        width={300}
+                        height={200}
               alt="Property 1"
             />
           </a>
           <div className="mt-2">
-            <p className="text-lg font-semibold">Property Title 1</p>
-            <p className="text-gray-600">Address: 123 Main St</p>
+                    <p className="text-lg font-semibold">{property.name}</p>
+            <p className="text-gray-600">Address: {property.location.street}, {property.location.city} </p>
           </div>
           <div className="mt-2">
             <a
@@ -32,33 +35,7 @@ const [properties, setproperties] = useState(intialProperties)
             </button>
           </div>
         </div>
-        <div className="mb-10">
-          <a href="/property.html">
-            <img
-              className="h-32 w-full rounded-md object-cover"
-              src="/images/properties/b1.jpg"
-              alt="Property 2"
-            />
-          </a>
-          <div className="mt-2">
-            <p className="text-lg font-semibold">Property Title 2</p>
-            <p className="text-gray-600">Address: 456 Elm St</p>
-          </div>
-          <div className="mt-2">
-            <a
-              href="/add-property.html"
-              className="bg-blue-500 text-white px-3 py-3 rounded-md mr-2 hover:bg-blue-600"
-            >
-              Edit
-            </a>
-            <button
-              className="bg-red-500 text-white px-3 py-2 rounded-md hover:bg-red-600"
-              type="button"
-            >
-              Delete
-            </button>
-          </div>
-        </div>
+         
       </>
     ));
     
