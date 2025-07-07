8bf2cab988e70c70dc9c1ca25cb32b6d8f53df7f
gemechuH <gemechuhunduma100@gmail.com>
2025-07-07 04:44:12 +0300

    edit form

--- app/properties/[id]/edit/page.jsx
+++ app/properties/[id]/edit/page.jsx
@@ -1,11 +1,24 @@
-import PropertyEditForm from "@/components/PropertyEditForm";
+import connectDB from "@/config/database";
 
-const EditPage = () => {
+import PropertyEditForm from "@/components/PropertyEditForm";
+import Property from "@/models/Property";
+import { convertToSerializableObject } from "@/utils/convertToObjectSerializable";
+
+const EditPage = async ({ params }) => {
+
+    await connectDB()
+    const propertyDoc =await Property.findById(params.id).lean()
+    const property =  convertToSerializableObject(propertyDoc)
+    if (!property)
+    return(
+        <h1 className="text-center text-2xl font-bold mt-10">Property Not Found</h1>
+    )
     return ( 
         <section className="bg-blue-50">
             <div className="container m-auto max-w-2xl py-24">
                 <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
-                    <PropertyEditForm/>
+                    {property.name}
+                    <PropertyEditForm property={property} />
                 </div>
             </div>
         </section>
