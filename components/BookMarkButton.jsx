ad65aa8af57a63ad0931e5c0f4280b4f4100c850
gemechuH <gemechuhunduma100@gmail.com>
2025-07-09 03:54:48 +0300

    more styles

--- components/BookMarkButton.jsx
+++ components/BookMarkButton.jsx
@@ -1,6 +1,10 @@
-const BookMarkButton = ({property}) => {
-    return ( <div>book mark</div>
-     );
+import {FaBookmark} from 'react-icons/fa'
+const BookMarkButton = ({ property }) => {
+    return (
+      <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center">
+       <FaBookmark class="fas fa-share mr-2"/> Bookmark Property
+      </button>
+    );
 }
  
 export default BookMarkButton;
--- components/ShareButton.jsx
+++ components/ShareButton.jsx
@@ -1,5 +1,11 @@
+import { FaShare } from "react-icons/fa";
 const ShareButton = ({ property }) => {
-  return <div>ShareButton</div>;
+
+    return (
+      <button class="bg-orange-500 hover:bg-orange-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center">
+       <FaShare class="fas fa-share mr-2"/> Share Property
+      </button>
+    );
 };
  
 export default ShareButton;
