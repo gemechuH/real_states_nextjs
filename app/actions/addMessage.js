9a4246a8434e548fb4b1081ec6b0a6ac0c30dec2
gemechuH <gemechuhunduma100@gmail.com>
2025-07-11 03:24:57 +0300

    addMessage action

---
+++ app/actions/addMessage.js
@@ -0,0 +1,38 @@
+'use server'
+import connectDB from "@/config/database";
+import Message from "@/models/message";
+
+import { getSessionUser } from "@/utils/getSessionUser";
+import Email from "next-auth/providers/email";
+
+
+async function addMessage(formData) {
+    await connectDB()
+    const sessinUser = await getSessionUser()
+    if (!sessinUser || !sessinUser.userId) {
+        throw new Error("User not authenticated");
+       
+    }
+    const { userId } = sessinUser;
+
+    const recipient = formData.get('recipient')
+    if (userId === recipient) {
+        return{error: 'you can not send a message to yourself'}
+    }
+
+    const newMessage = new Message ({
+      sender: userId,
+      recipient,
+      property: formData.get("property"),
+
+      name: formData.get("name"),
+      Email: formData.get("email"),
+      phone: formData.get("phone"),
+      body: formData.get("body"),
+    });
+
+    await newMessage.save()
+    return {submitted: true}
+
+}
+export default addMessage
