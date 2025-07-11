8202f3d583c05c7a84a05068d3401bfdd8b49799
gemechuH <gemechuhunduma100@gmail.com>
2025-07-11 03:11:10 +0300

    message model

---
+++ models/message.js
@@ -0,0 +1,45 @@
+import { Schema, model, models } from "mongoose";
+
+const MessageSchema = new Schema(
+  {
+    sender: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "Sender is required"],
+    },
+    recipient: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "Recipient is required"],
+        },
+        property: {
+          type: Schema.Types.ObjectId,
+          ref: "Property",
+          required: [true, "Property is required"],
+        },
+        name:{
+          type: String,
+          required: [true, 'name is required']
+    },
+        email:{
+          type: String,
+          required: [true, 'email is required']
+    },
+    phone: {
+          type: String
+    },
+    body: String,
+    read: String,
+
+        
+    // content: {
+    //   type: String,
+    //   required: [true, "Message content is required"],
+    // },
+  },
+  {
+    timestamps: true,
+  }
+);
+const Message = models.Message || model("Message", MessageSchema);
+export default Message;
